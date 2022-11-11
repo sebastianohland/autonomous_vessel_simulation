Fixed line intersection formula, added obs module

classes/State.py
@@ -6,24 +6,19 @@ from tools import theta_to_coord
 
 class State:
 
-    # TODO: add state attributes (ex TSS, restricted vis, narrow channel, etc.) (DO THIS FOR EACH INDIVIDUAL VESSEL!)
-    # TODO: read in existing state
-
     def __init__(self, state=None):
         if state is None:
-            state = {}
+            state = {"ownship": [], "targetships": []}
         self.state = state
-        self.state["ownvessel"] = []
-        self.state["targetvessels"] = []
 
-    def add_target_vessel(self, target_vessel):
+    def add_target_ship(self, target_vessel):
         tv_state = target_vessel.get_state()
-        tv_state["id"] = "tv" + str(len(self.state["targetvessels"]) + 1)
-        self.state["targetvessels"].append(tv_state)
+        tv_state["id"] = "ts" + str(len(self.state["targetships"]) + 1)
+        self.state["targetships"].append(tv_state)
 
-    def add_own_vessel(self, own_vessel):
+    def add_own_ship(self, own_vessel):
         ov_state = own_vessel.get_state()
-        self.state["ownvessel"] = ov_state
+        self.state["ownship"] = ov_state
 
     def get_state(self):
         return self.state
@@ -57,46 +52,47 @@ class State:
         ax.set_yticks(np.linspace(-scale, scale, 2 * scale + 1))
 
         # plot own vessel
-        x = self.state["ownvessel"]["xpos"]
-        y = self.state["ownvessel"]["ypos"]
-        xvector, yvector = theta_to_coord(self.state["ownvessel"]["cog"], self.state["ownvessel"]["sog"])
+        x = self.state["ownship"]["xpos"]
+        y = self.state["ownship"]["ypos"]
+        xvector, yvector = theta_to_coord(self.state["ownship"]["cog"], self.state["ownship"]["sog"])
         ax.scatter(x, y, color='blue')
         ax.arrow(x, y, xvector, yvector, head_width=0.4, head_length=0.4, lw=1, fc='blue', ec='blue', length_includes_head=True)
 
         # plot target vessels
-        for i in range(len(self.state["targetvessels"])):
-            x = self.state["targetvessels"][i]["xpos"]
-            y = self.state["targetvessels"][i]["ypos"]
-            xvector, yvector = theta_to_coord(self.state["targetvessels"][i]["cog"], self.state["targetvessels"][i]["sog"])
+        for i in range(len(self.state["targetships"])):
+            x = self.state["targetships"][i]["xpos"]
+            y = self.state["targetships"][i]["ypos"]
+            xvector, yvector = theta_to_coord(self.state["targetships"][i]["cog"], self.state["targetships"][i]["sog"])
             ax.scatter(x, y, color='red')
             ax.arrow(x, y, xvector, yvector, head_width=0.2, head_length=0.4, lw=1, fc='red', ec='red', length_includes_head=True)
+
             # add encounter situation after id if parameter is given
             if encounter_situations:
-                ax.annotate(self.state["targetvessels"][i]["id"] + str(encounter_situations[i]), (x + 0.5, y))
+                ax.annotate(self.state["targetships"][i]["id"] + str(encounter_situations[i]), (x + 0.5, y))
             else:
-                ax.annotate(self.state["targetvessels"][i]["id"], (x, y))
+                ax.annotate(self.state["targetships"][i]["id"], (x, y))
 
         # plot collision parameters as table if argument is given
         if collision_parameters:
-            data = np.array(collision_parameters)
-            # converting tcpa and tcbr to minutes
-            for i in data:
-                i[2] = i[2] * 60
-                i[4] = i[4] * 60
-            data = np.round(data, 2)
-            columns = ["rng (nm)", "dcpa", "tcpa", "bcr", "tbcr", "Q", "Q1", "v_rat"]
-            rows = [i["id"] for i in self.state["targetvessels"]]
-            table = ax2.table(cellText=data, rowLabels=rows, colLabels=columns, loc="center")
+            columns = ["rng", "dcpa", "tcpa", "bcr", "tbcr", "Q", "Q1", "v_rat"]
+            rows = [i["id"] for i in self.state["targetships"]]
+            table = ax2.table(cellText=collision_parameters, rowLabels=rows, colLabels=columns, loc="center")
             table.scale(1, 1.7)
 
         plt.show()
 
     def next_state(self, timefactor):
-        for i in self.state["targetvessels"]:
+        for i in self.state["targetships"]:
             new_x, new_y = theta_to_coord(i["cog"], i["sog"] / timefactor)
             i["xpos"] = new_x + i["xpos"]
             i["ypos"] = new_y + i["ypos"]
-        new_x, new_y = theta_to_coord(self.state["ownvessel"]["cog"], self.state["ownvessel"]["sog"] / timefactor)
-        self.state["ownvessel"]["xpos"] = new_x + self.state["ownvessel"]["xpos"]
-        self.state["ownvessel"]["ypos"] = new_y + self.state["ownvessel"]["ypos"]
+        new_x, new_y = theta_to_coord(self.state["ownship"]["cog"], self.state["ownship"]["sog"] / timefactor)
+        self.state["ownship"]["xpos"] = new_x + self.state["ownship"]["xpos"]
+        self.state["ownship"]["ypos"] = new_y + self.state["ownship"]["ypos"]
         return self
+
+
+def read_state(path):
+    with open(path, "r") as f:
+        data = json.load(f)
+    return State(data)

modules/collision_parameter_module.py
@@ -38,50 +38,57 @@ def calculate_collision_parameters(os_pos, os_cog, os_sog, os_len, ts_pos, ts_co
     # TODO: check bcr calculations
 
     # calculate absolute position of velocity vectors
-    os_abs_vel = (os_pos[0] + os_vel[0], os_pos[1] + os_vel[1])
-    ts_abs_vel = (ts_pos[0] + ts_vel[0], ts_pos[1] + ts_vel[1])
+    os_vel_pos = (os_pos[0] + os_vel[0], os_pos[1] + os_vel[1])
+    ts_vel_pos = (ts_pos[0] + ts_vel[0], ts_pos[1] + ts_vel[1])
 
     # calculate bow cross position (intersecting lines)
-    ts_bcr_pos = line_intersect(os_pos[0], os_pos[1], os_abs_vel[0], os_abs_vel[1], ts_pos[0], ts_pos[1], ts_abs_vel[0], ts_abs_vel[1])
+    ts_bcr_pos = line_intersect(os_pos, os_vel_pos, ts_pos, ts_vel_pos)
+    #print("TS bow cross pos x: {}   y: {}".format(round(ts_bcr_pos[0], 2), round(ts_bcr_pos[1], 2)))
 
-    # time to BCR
-    tbcr = np.sqrt((ts_pos[0] - ts_bcr_pos[0]) ** 2 + (ts_pos[1] - ts_bcr_pos[1]) ** 2) / np.abs(ts_sog)
+    if ts_bcr_pos:
 
-    # help variables
-    os_u_x = os_vel[0] / np.abs(os_sog)
-    os_u_y = os_vel[1] / np.abs(os_sog)
+        # time to BCR
+        tbcr = np.sqrt((ts_pos[0] - ts_bcr_pos[0]) ** 2 + (ts_pos[1] - ts_bcr_pos[1]) ** 2) / np.abs(ts_sog)
 
-    # converts ship length to nm
-    os_len = m_to_nm(os_len)
-    ts_len = m_to_nm(ts_len)
-    # pos of os bow at point of bcr
-    os_bcr_xpos = os_pos[0] + (os_len / 2) * os_u_x + os_vel[0] * tbcr
-    os_bcr_ypos = os_pos[1] + (os_len / 2) * os_u_y + os_vel[1] * tbcr
+        # help variables
+        os_u_x = os_vel[0] / np.abs(os_sog)
+        os_u_y = os_vel[1] / np.abs(os_sog)
 
-    # BCR calculation
-    bcr = np.sqrt((ts_bcr_pos[0] - os_bcr_xpos) ** 2 + (ts_bcr_pos[1] - os_bcr_ypos) ** 2)
+        # converts ship length to nm
+        os_len = m_to_nm(os_len)
+        ts_len = m_to_nm(ts_len)
+        # pos of os bow at point of bcr
+        os_bcr_xpos = os_pos[0] + (os_len / 2) * os_u_x + os_vel[0] * tbcr
+        os_bcr_ypos = os_pos[1] + (os_len / 2) * os_u_y + os_vel[1] * tbcr
 
-    # help variables for BCR sign calculation
-    ts_u_x = ts_vel[0] / np.abs(ts_sog)
-    ts_u_y = ts_vel[1] / np.abs(ts_sog)
+        # BCR calculation
+        bcr = np.sqrt((ts_bcr_pos[0] - os_bcr_xpos) ** 2 + (ts_bcr_pos[1] - os_bcr_ypos) ** 2)
 
-    # current pos of ts fore and aft
-    ts_fore_xpos = ts_pos[0] + (ts_len / 2) * ts_u_x
-    ts_fore_ypos = ts_pos[1] + (ts_len / 2) * ts_u_y
-    ts_aft_xpos = ts_pos[0] - (ts_len / 2) * ts_u_x
-    ts_aft_ypos = ts_pos[1] - (ts_len / 2) * ts_u_x
+        # help variables for BCR sign calculation
+        ts_u_x = ts_vel[0] / np.abs(ts_sog)
+        ts_u_y = ts_vel[1] / np.abs(ts_sog)
 
-    # BCR sign calculation. Checks if ts fore or aft is closer to point of BCR
-    bcr_distance_fore = np.sqrt((ts_fore_xpos - ts_bcr_pos[0]) ** 2 + (ts_fore_ypos - ts_bcr_pos[1]) ** 2)
-    bcr_distance_aft = np.sqrt((ts_aft_xpos - ts_bcr_pos[0]) ** 2 + (ts_aft_ypos - ts_bcr_pos[1]) ** 2)
-    bcr_sign = 0
-    if bcr_distance_fore <= bcr_distance_aft:
-        bcr_sign = 1
-    elif bcr_distance_fore > bcr_distance_aft:
-        bcr_sign = -1
+        # current pos of ts fore and aft
+        ts_fore_xpos = ts_pos[0] + (ts_len / 2) * ts_u_x
+        ts_fore_ypos = ts_pos[1] + (ts_len / 2) * ts_u_y
+        ts_aft_xpos = ts_pos[0] - (ts_len / 2) * ts_u_x
+        ts_aft_ypos = ts_pos[1] - (ts_len / 2) * ts_u_x
 
-    # adds sign to bcr
-    tbcr = tbcr * bcr_sign
+        # BCR sign calculation. Checks if ts fore or aft is closer to point of BCR
+        bcr_distance_fore = np.sqrt((ts_fore_xpos - ts_bcr_pos[0]) ** 2 + (ts_fore_ypos - ts_bcr_pos[1]) ** 2)
+        bcr_distance_aft = np.sqrt((ts_aft_xpos - ts_bcr_pos[0]) ** 2 + (ts_aft_ypos - ts_bcr_pos[1]) ** 2)
+        bcr_sign = 0
+        if bcr_distance_fore <= bcr_distance_aft:
+            bcr_sign = 1
+        elif bcr_distance_fore > bcr_distance_aft:
+            bcr_sign = -1
+
+        # adds sign to bcr
+        tbcr = tbcr * bcr_sign
+
+    else:
+        bcr = np.NaN
+        tbcr = np.NaN
 
     collision_parameters.append(rng)
     collision_parameters.append(dcpa)
@@ -92,39 +99,41 @@ def calculate_collision_parameters(os_pos, os_cog, os_sog, os_len, ts_pos, ts_co
     collision_parameters.append(Q1)
     collision_parameters.append(v_rat)
 
+    collision_parameters = np.around(collision_parameters, 2)
+
     return collision_parameters
 
 
 def calculate_state_collision_parameters(state):
 
-    os_pos = (state["ownvessel"]["xpos"], state["ownvessel"]["ypos"])
-    os_cog = state["ownvessel"]["cog"]
-    os_sog = state["ownvessel"]["sog"]
-    os_length = state["ownvessel"]["length"]
+    os_pos = (state["ownship"]["xpos"], state["ownship"]["ypos"])
+    os_cog = state["ownship"]["cog"]
+    os_sog = state["ownship"]["sog"]
+    os_length = state["ownship"]["length"]
 
-    all_coll_par = []
+    state_collision_parameters = []
 
-    for i in state["targetvessels"]:
+    for i in state["targetships"]:
         ts_pos = (i["xpos"], i["ypos"])
         ts_cog = i["cog"]
         ts_sog = i["sog"]
         ts_length = i["length"]
 
-        col_par = calculate_collision_parameters(os_pos, os_cog, os_sog, os_length, ts_pos, ts_cog, ts_sog, ts_length)
+        collision_parameters = calculate_collision_parameters(os_pos, os_cog, os_sog, os_length, ts_pos, ts_cog, ts_sog, ts_length)
+        state_collision_parameters.append(collision_parameters)
 
-        all_coll_par.append(col_par)
-
-    return all_coll_par
+    return state_collision_parameters
 
 
 def print_collision_parameters(coll_parameters):
     for j in range(len(coll_parameters)):
-        print("Range: {:0.2f} || DCPA: {:0.2f} || TCPA: {:0.2f} || BCR: {:0.2f} || TBCR: {:0.2f} || Q: {:0.2f} || Q1: {:0.2f} || v_rat: {:0.2f}"
+        print("Range: {:0.2f} || DCPA: {:0.2f} || TCPA: {:0.2f} || BCR: {:0.2f} || TBCR: {:0.2f} "
+              "|| Q: {:0.2f} || Q1: {:0.2f} || v_rat: {:0.2f}"
               .format(coll_parameters[j][0],
                       coll_parameters[j][1],
-                      coll_parameters[j][2] * 60,
+                      coll_parameters[j][2],
                       coll_parameters[j][3],
-                      coll_parameters[j][4] * 60,
+                      coll_parameters[j][4],
                       coll_parameters[j][5],
                       coll_parameters[j][6],
                       coll_parameters[j][7]

tools.py
@@ -2,29 +2,33 @@ import numpy as np
 
 
 def theta_to_coord(theta, d):
-    x = d * np.sin(np.radians(theta))
-    y = d * np.cos(np.radians(theta))
+    x = np.round(d * np.sin(np.radians(theta)), 5)
+    y = np.round(d * np.cos(np.radians(theta)), 5)
     return x, y
 
 
 def coord_to_theta(x, y):
-    theta = (360 + np.degrees(np.arctan2(x, y))) % 360
+    theta = np.round((360 + np.degrees(np.arctan2(x, y))) % 360, 5)
     return theta
 
 
-def line_intersect(Ax1, Ay1, Ax2, Ay2, Bx1, By1, Bx2, By2):
-    """ returns a (x, y) tuple or None if there is no intersection, from rosettacode.org"""
-    d = (By2 - By1) * (Ax2 - Ax1) - (Bx2 - Bx1) * (Ay2 - Ay1)
-    if d:
-        uA = ((Bx2 - Bx1) * (Ay1 - By1) - (By2 - By1) * (Ax1 - Bx1)) / d
-        uB = ((Ax2 - Ax1) * (Ay1 - By1) - (Ay2 - Ay1) * (Ax1 - Bx1)) / d
-    else:
-        return -1, -1   # TODO: WHAT TO RETURN???
-    #if not (0 <= uA <= 1 and 0 <= uB <= 1):
-    #    return -1, -1   # TODO: WTF?
-    x = Ax1 + uA * (Ax2 - Ax1)
-    y = Ay1 + uA * (Ay2 - Ay1)
+def line_intersect(A1, A2, B1, B2):
+    """Returns intersection coordinates of lines A and B or None if there is no intersection.
+    Source from: https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines"""
 
+    xdiff = (A1[0] - A2[0], B1[0] - B2[0])
+    ydiff = (A1[1] - A2[1], B1[1] - B2[1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+        return None
+
+    d = (det(A1, A2), det(B1, B2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
     return x, y
 
 